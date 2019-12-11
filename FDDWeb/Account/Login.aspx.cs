105f787592494574f7cdc67843974d73ba3a1703
Subhadeep Dey <subhadeepdey@hotmail.com>
2019-12-10 22:53:41 -0700

    Working

--- FDDWeb/Account/Login.aspx.cs
+++ FDDWeb/Account/Login.aspx.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Owin;
 using FDDWeb.Models;
+using Authentication.DAL;
+using System.Web.Security;
 
 namespace FDDWeb.Account
 {
@@ -25,37 +27,65 @@ namespace FDDWeb.Account
 
         protected void LogIn(object sender, EventArgs e)
         {
-            if (IsValid)
+            if (!IsValid)
             {
-                // Validate the user password
-                var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
-                var signinManager = Context.GetOwinContext().GetUserManager<ApplicationSignInManager>();
-
-                // This doen't count login failures towards account lockout
-                // To enable password failures to trigger lockout, change to shouldLockout: true
-                var result = signinManager.PasswordSignIn(Email.Text, Password.Text, RememberMe.Checked, shouldLockout: false);
-
-                switch (result)
-                {
-                    case SignInStatus.Success:
-                        IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
-                        break;
-                    case SignInStatus.LockedOut:
-                        Response.Redirect("/Account/Lockout");
-                        break;
-                    case SignInStatus.RequiresVerification:
-                        Response.Redirect(String.Format("/Account/TwoFactorAuthenticationSignIn?ReturnUrl={0}&RememberMe={1}", 
-                                                        Request.QueryString["ReturnUrl"],
-                                                        RememberMe.Checked),
-                                          true);
-                        break;
-                    case SignInStatus.Failure:
-                    default:
-                        FailureText.Text = "Invalid login attempt";
-                        ErrorMessage.Visible = true;
-                        break;
-                }
+                return ;
             }
+
+            User user = new User() { Email = Email.Text, Password = Password.Text };
+
+            user = Repository.GetUserDetails(user);
+
+            if (user != null)
+            {
+                FormsAuthentication.SetAuthCookie(Email.Text, false);
+
+                var authTicket = new FormsAuthenticationTicket(1, user.Email, DateTime.Now, DateTime.Now.AddMinutes(20), false, user.Roles);
+                string encryptedTicket = FormsAuthentication.Encrypt(authTicket);
+                var authCookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
+                HttpContext.Current.Response.Cookies.Add(authCookie);
+                IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+                //Response.Redirect("/Home");
+            }
+
+            else
+            {
+                FailureText.Text = "Invalid login attempt";
+                ErrorMessage.Visible = true;
+                Response.Redirect("/Account/Login");
+            }
+
+            //if (IsValid)
+            //{
+            //    // Validate the user password
+            //    var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            //    var signinManager = Context.GetOwinContext().GetUserManager<ApplicationSignInManager>();
+
+            //    // This doen't count login failures towards account lockout
+            //    // To enable password failures to trigger lockout, change to shouldLockout: true
+            //    var result = signinManager.PasswordSignIn(Email.Text, Password.Text, RememberMe.Checked, shouldLockout: false);
+
+            //    switch (result)
+            //    {
+            //        case SignInStatus.Success:
+            //            IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+            //            break;
+            //        case SignInStatus.LockedOut:
+            //            Response.Redirect("/Account/Lockout");
+            //            break;
+            //        case SignInStatus.RequiresVerification:
+            //            Response.Redirect(String.Format("/Account/TwoFactorAuthenticationSignIn?ReturnUrl={0}&RememberMe={1}", 
+            //                                            Request.QueryString["ReturnUrl"],
+            //                                            RememberMe.Checked),
+            //                              true);
+            //            break;
+            //        case SignInStatus.Failure:
+            //        default:
+            //            FailureText.Text = "Invalid login attempt";
+            //            ErrorMessage.Visible = true;
+            //            break;
+            //    }
+            //}
         }
     }
 }
--- FDDWeb/Global.asax.cs
+++ FDDWeb/Global.asax.cs
@@ -17,5 +17,18 @@ namespace FDDWeb
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
+        {
+            var authCookie = HttpContext.Current.Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie != null)
+            {
+                FormsAuthenticationTicket authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+                if (authTicket != null && !authTicket.Expired)
+                {
+                    var roles = authTicket.UserData.Split(',');
+                    HttpContext.Current.User = new System.Security.Principal.GenericPrincipal(new FormsIdentity(authTicket), roles);
+                }
+            }
+        }
     }
 }
